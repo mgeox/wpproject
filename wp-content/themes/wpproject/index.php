f12797bcdc5c82cf5f9b3be6bc3e0c5487d65f50
mgeox <ungurean.geta@yahoo.com>
2021-06-15 17:49:31 +0300

    started working on homepage

--- wp-content/themes/wpproject/index.php
+++ wp-content/themes/wpproject/index.php
@@ -1,3 +1,9 @@
 <?php get_header() ?>
 
+<?php get_template_part("template_parts/homepage/trending");?>
+<?php get_template_part("template_parts/homepage/breaking_news");?>
+<?php get_template_part("template_parts/homepage/latest_stories");?>
+<?php get_template_part("template_parts/homepage/editor's picks");?>
+
+
 <?php get_footer() ?>
---
+++ wp-content/themes/wpproject/template_parts/homepage/breaking_news.php
@@ -0,0 +1,6 @@
+
+<div class="breaking_news">
+    <button> Breaking News </button>
+    <p>     Kanye West says he's running for president in 2020 </p>
+</div>
+
