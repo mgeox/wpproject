8a0916fd23147362daa190fcf6582284d9c5d1f8
mgeox <ungurean.geta@yahoo.com>
2021-07-11 02:20:52 +0300

    added news cards

---
+++ wp-content/themes/wpproject/template_parts/homepage/editors_picks.php
@@ -0,0 +1,6 @@
+<div class="editor-picks">
+    <p>Editor's picks  <i class="fas fa-star" aria-hidden="true"></i></p>
+
+
+    
+</div>
---
+++ wp-content/themes/wpproject/template_parts/homepage/latest_stories.php
@@ -0,0 +1,130 @@
+<!-- <div class="grid">
+<div class='box'>
+      <div class='box'>
+        <div class="article" > 
+            <img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p>
+        
+        </div> 
+        <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+    </div>
+    </div>
+  </div>
+</div>
+
+      <div class='box'>
+        <div class="article" > 
+            <img src="<?= get_stylesheet_directory_uri() . "/images/jlewis2.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p>
+        
+        </div> 
+        <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+    </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</div> -->
+
+
+
+
+
+<section id="grid">
+
+<div class="box">
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p></div>
+            <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+</div>
+
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p></div>
+            <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+</div>
+
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p></div>
+            <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+</div>
+
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p></div>
+            <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+</div>
+
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
+            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+        <div class="article-details">
+            <p> 2 hours ago </p>
+            <p> By Lucy Hiddleston </p>
+            <p> 4 min read </p></div>
+            <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+</div>
+
+</div>
+</div>
+
+</section>
+
