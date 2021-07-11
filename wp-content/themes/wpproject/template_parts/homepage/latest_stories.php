63065856b8f2c1695575a9b13e60e4cd97098b27
mgeox <ungurean.geta@yahoo.com>
2021-07-11 14:09:29 +0300

    updated grid

--- wp-content/themes/wpproject/template_parts/homepage/latest_stories.php
+++ wp-content/themes/wpproject/template_parts/homepage/latest_stories.php
@@ -1,56 +1,20 @@
-<!-- <div class="grid">
-<div class='box'>
-      <div class='box'>
-        <div class="article" > 
-            <img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
-            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
-            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
-        <div class="article-details">
-            <p> 2 hours ago </p>
-            <p> By Lucy Hiddleston </p>
-            <p> 4 min read </p>
-        
-        </div> 
-        <div class="popularity">  
-                        <div class="social-icons"><i class='far fa-heart'></i>
-                        <i class="fa fa-bookmark-o"></i>
-                        <i class='fas fa-share-square'></i></div>
-            </div>
-    </div>
-    </div>
-  </div>
-</div>
-
-      <div class='box'>
-        <div class="article" > 
-            <img src="<?= get_stylesheet_directory_uri() . "/images/jlewis2.png" ?>">
-            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
-            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
-        <div class="article-details">
-            <p> 2 hours ago </p>
-            <p> By Lucy Hiddleston </p>
-            <p> 4 min read </p>
-        
-        </div> 
-        <div class="popularity">  
-                        <div class="social-icons"><i class='far fa-heart'></i>
-                        <i class="fa fa-bookmark-o"></i>
-                        <i class='fas fa-share-square'></i></div>
-            </div>
-    </div>
-    </div>
-  </div>
-</div>
-
-
-</div>
-</div> -->
+<head>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@600&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
+</head>
 
 
 
 
 
 <section id="grid">
+
+<div class="categories">
+    <p>Latest Stories</p>
+    <p>Think</p>
+    <p>Health</p>
+    <p class="menu"> <i class="fa fa-bars" aria-hidden="true"></i></p>
+</div>
 
 <div class="box">
     <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
@@ -60,6 +24,8 @@
             <p> 2 hours ago </p>
             <p> By Lucy Hiddleston </p>
             <p> 4 min read </p></div>
+            <br>
+            <hr>
             <div class="popularity">  
                         <div class="social-icons"><i class='far fa-heart'></i>
                         <i class="fa fa-bookmark-o"></i>
@@ -67,13 +33,47 @@
             </div>
 </div>
 
-    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
-            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis2.png" ?>">
+            <h1> John Lewis, civil rights giant, crosses infamous Selma bridge one final time</h1>
+            <h3>Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge</h3>
+        <div class="article-details">
+            <p> 4 hours ago </p>
+            <p> By Caroline Casey </p>
+            <p> 4 min read </p></div>
+            <br>
+            <br>
+            <hr>
+            <div class="popularity">  
+                        <div class="social-icons"><i class='far fa-heart'></i>
+                        <i class="fa fa-bookmark-o"></i>
+                        <i class='fas fa-share-square'></i></div>
+            </div>
+</div>
+
+<div class="box-content">
+    <p> Live  &#128308 </p><img src="<?= get_stylesheet_directory_uri() . "/images/coach.png" ?>">
+            <h1> Beloved Arizona coach loses battle with coronavirus</h1>
+            <h3>Location News</h3>
+        <div class="location-news">
+        <h3>Get location specific News</h3>
+        <div class="search">
+      <input type="text" placeholder="Enter your Location">
+      <br>
+      <button type="submit">
+        SUBMIT
+     </button>
+     </div>
+</div>
+</div>
+
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/tornado.png" ?>">
+            <h1> Tornado and tide warnings as Storm Hanna lashes Texas</h1>
             <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
         <div class="article-details">
-            <p> 2 hours ago </p>
+            <p> 5 hours ago </p>
             <p> By Lucy Hiddleston </p>
             <p> 4 min read </p></div>
+            <hr>
             <div class="popularity">  
                         <div class="social-icons"><i class='far fa-heart'></i>
                         <i class="fa fa-bookmark-o"></i>
@@ -81,41 +81,15 @@
             </div>
 </div>
 
-    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
-            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
-            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
+    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/stevejobs.png" ?>">
+            <h1> 20 Years Ago, Steve Jobs Built the ‘Coolest Computer Ever.’ It Bombed</h1>
+            <h3>This month marks the 20th anniversary of the Power Mac G4 Cube, which debuted July 19, 2000. It also marks the 19th anniversary of Apple’s announcement that it was putting the Cube on ice.</h3>
         <div class="article-details">
-            <p> 2 hours ago </p>
+            <p> 12 hours ago </p>
             <p> By Lucy Hiddleston </p>
+            
             <p> 4 min read </p></div>
-            <div class="popularity">  
-                        <div class="social-icons"><i class='far fa-heart'></i>
-                        <i class="fa fa-bookmark-o"></i>
-                        <i class='fas fa-share-square'></i></div>
-            </div>
-</div>
-
-    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
-            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
-            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
-        <div class="article-details">
-            <p> 2 hours ago </p>
-            <p> By Lucy Hiddleston </p>
-            <p> 4 min read </p></div>
-            <div class="popularity">  
-                        <div class="social-icons"><i class='far fa-heart'></i>
-                        <i class="fa fa-bookmark-o"></i>
-                        <i class='fas fa-share-square'></i></div>
-            </div>
-</div>
-
-    <div class="box-content"><img src="<?= get_stylesheet_directory_uri() . "/images/jlewis.png" ?>">
-            <h1> John Lewis to make final journey across Edmund Pettus Bridge in procession</h1>
-            <h3>The body of the late US Rep. John Lewis on Sunday will make the final journey across the famous bridge in Selma, Alabama, where he helped lead a march for voting rights in 1965.</h3>
-        <div class="article-details">
-            <p> 2 hours ago </p>
-            <p> By Lucy Hiddleston </p>
-            <p> 4 min read </p></div>
+            <hr>
             <div class="popularity">  
                         <div class="social-icons"><i class='far fa-heart'></i>
                         <i class="fa fa-bookmark-o"></i>
@@ -124,7 +98,15 @@
 </div>
 
 </div>
+
+
 </div>
+
 
 </section>
 
+<div class="view-more">
+<button type="submit">
+        VIEW MORE
+     </button>
+</div>
